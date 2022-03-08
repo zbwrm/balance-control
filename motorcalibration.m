05b2eb72f96dc8575c2125036ada0f4cbadc5031
Aydin O'Leary <aydinoleary@gmail.com>
2022-03-08 14:34:09 -0500

    attempt to curvefit motor test data -- please test!

--- motorcalibration.m
+++ motorcalibration.m
@@ -1,9 +1,16 @@
 clear all;
 motordata = csvread("motortest2.csv");
 
+sample_rate = 100; % Hertz
+step = (1:80)' - 1;
+timestep = (step ./ sample_rate);
 leftmotor = motordata(:,1);
 rightmotor = motordata(:,2);
 
-lcroppeddata = leftmotor(1:80,:);
-rcroppeddata = rightmotor(1:80,:);
-step_for_cftool = (1:length(lcroppeddata))';
+plot(timestep, leftmotor(1:80))
+leftfit = fit(timestep, leftmotor(1:80), 'exp1');
+
+
+% lcroppeddata = leftmotor(1:80,:);
+% rcroppeddata = rightmotor(1:80,:);
+% step_for_cftool = (1:length(lcroppeddata))';
