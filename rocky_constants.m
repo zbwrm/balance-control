7bb63adc70012c64f5bce42a095c2ceaf8331f75
Aydin O'Leary <aydinoleary@gmail.com>
2022-03-11 14:19:19 -0500

    Grab simulink model from Canvas

---
+++ rocky_constants.m
@@ -0,0 +1,25 @@
+% rocky
+% disturbance rejection
+% critically damped
+%
+clear all close all 
+
+l = 0.4;
+g = 9.8;
+
+
+% motor
+kmotor = .0025;
+tau = .05;
+
+
+% pole = 10;
+% kp = pole*l;
+% ki = (kp^2/l + 4*g)/4;
+
+kp = 1000
+ki = 8800
+
+
+% kp = 20
+% ki = 100
