1c9b9b275e79e403980669bb348a697bc80ae764
Aydin O'Leary <aydinoleary@gmail.com>
2022-03-10 22:29:13 -0500

    Add curve-fitting to motor calibration

--- motorcalibration.m
+++ motorcalibration.m
@@ -1,16 +1,23 @@
 clear all;
-motordata = csvread("motortest2.csv");
+motordata = readmatrix("motortest2.csv");
 
 sample_rate = 100; % Hertz
 step = (1:80)' - 1;
 timestep = (step ./ sample_rate);
 leftmotor = motordata(:,1);
 rightmotor = motordata(:,2);
+expfittype = fittype("a-a*exp(-b*x)");
 
-plot(timestep, leftmotor(1:80))
-leftfit = fit(timestep, leftmotor(1:80), 'exp1');
+figure();
+title("Motor Calibration")
+subplot(1,2,1);
+plot(timestep, leftmotor(1:80,:)); hold on
+leftfit = fit(timestep, leftmotor(1:80), expfittype);
+plot(leftfit); text(0.45,0.04,sprintf('a=%.3f, b=%.4f', leftfit.a, 1/leftfit.b)); 
+title('Left motor'); ylabel("Motor Speed"); xlabel("Time (s)")
 
-
-% lcroppeddata = leftmotor(1:80,:);
-% rcroppeddata = rightmotor(1:80,:);
-% step_for_cftool = (1:length(lcroppeddata))';
+subplot(1,2,2);
+plot(timestep, rightmotor(1:80,:)); hold on
+rightfit = fit(timestep, rightmotor(1:80), expfittype);
+plot(rightfit); text(0.45,0.04,sprintf('a=%.3f, b=%.4f', rightfit.a, 1/rightfit.b)); 
+title("Right Motor"); ylabel("Motor Speed"); xlabel("Time (s)")
