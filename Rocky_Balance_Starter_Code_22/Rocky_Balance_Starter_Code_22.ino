4681f55e4cb37fd62b1670d124de509dfdc89c5b
Maya <mayalum8@gmail.com>
2022-03-11 15:21:14 -0500

    Downloaded and added closed loop poles calculator

--- Rocky_Balance_Starter_Code_22/Rocky_Balance_Starter_Code_22.ino
+++ Rocky_Balance_Starter_Code_22/Rocky_Balance_Starter_Code_22.ino
@@ -68,7 +68,7 @@ Balboa32U4Buzzer buzzer;
 Balboa32U4ButtonA buttonA;
 
 
-#define FIXED_ANGLE_CORRECTION (0.26)  // ***** Replace the value 0.25 with the value you obtained from the Gyro calibration procedure
+#define FIXED_ANGLE_CORRECTION (0.27)  // ***** Replace the value 0.25 with the value you obtained from the Gyro calibration procedure
 
 
 
